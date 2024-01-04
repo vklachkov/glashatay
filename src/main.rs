77838b6e30ac91e89bf09d2f5d9c166a9f957868
Valery Klachkov <zgdump@gmail.com>
2024-01-04 23:15:24 +0300

    Экспериментальный telegram bot, отправляющий один пост из ВК в канал

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,44 @@
 use anyhow::Context;
-use std::process;
-use tokio::fs;
+use std::{env, process, sync::Arc};
+use teloxide::{
+    dispatching::{
+        dialogue::{self, InMemStorage},
+        UpdateHandler,
+    },
+    macros::BotCommands,
+    prelude::*,
+    types::{Chat, ChatKind, ChatPublic, ForwardedFrom},
+};
 use url::Url;
+
+type MyDialogue = Dialogue<BotState, InMemStorage<BotState>>;
+type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
+
+pub struct GlobalState {}
+
+#[derive(Clone)]
+pub enum BotState {
+    Start,
+    ReceiveChannelId,
+    ReceiveVkUrl { channel_id: ChatId },
+    Active { channel_id: ChatId, vk_id: String },
+}
+
+impl Default for BotState {
+    fn default() -> Self {
+        Self::Active {
+            channel_id: ChatId(-1002042632833),
+            vk_id: "dar.viardo".to_owned(),
+        }
+    }
+}
+
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase")]
+enum BotCommand {
+    Start,
+    TestPost,
+}
 
 #[tokio::main]
 async fn main() {
@@ -28,6 +65,168 @@ fn hello() {
 }
 
 async fn run() -> anyhow::Result<()> {
+    const TOKEN: &str = "6786939573:AAEF6ciF_gBzKZzhsVV35qsYUgzfzX5IOA0";
+
+    env::set_var("TELOXIDE_TOKEN", TOKEN);
+    let bot = Bot::from_env();
+
+    let bot_state = InMemStorage::<BotState>::new();
+    let global_state = Arc::new(GlobalState {});
+
+    Dispatcher::builder(bot, schema())
+        .dependencies(dptree::deps![bot_state, global_state])
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch()
+        .await;
+
+    Ok(())
+}
+
+fn schema() -> UpdateHandler<Box<dyn std::error::Error + Send + Sync + 'static>> {
+    use dptree::case;
+
+    let command_handler = teloxide::filter_command::<BotCommand, _>()
+        .branch(case![BotState::Start].branch(case![BotCommand::Start].endpoint(bot_start)))
+        .branch(
+            case![BotState::Active { channel_id, vk_id }]
+                .branch(case![BotCommand::TestPost].endpoint(bot_test_post)),
+        );
+
+    let message_handler = Update::filter_message()
+        .branch(command_handler)
+        .branch(case![BotState::Start].endpoint(bot_lets_start))
+        .branch(case![BotState::ReceiveChannelId].endpoint(bot_receive_channel_id))
+        .branch(case![BotState::ReceiveVkUrl { channel_id }].endpoint(bot_receive_vk_url))
+        .branch(dptree::endpoint(bot_other));
+
+    dialogue::enter::<Update, InMemStorage<BotState>, BotState, _>().branch(message_handler)
+}
+
+async fn bot_lets_start(bot: Bot, msg: Message) -> HandlerResult {
+    let text = "Чтобы начать, введите /start";
+    bot.send_message(msg.chat.id, text).await?;
+
+    Ok(())
+}
+
+async fn bot_start(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
+    let text = "Привет! Перешли, пожалуйста, сообщение из паблика, в который будут отправляться посты из ВКонтакте";
+    bot.send_message(msg.chat.id, text).await?;
+
+    dialogue.update(BotState::ReceiveChannelId).await?;
+
+    Ok(())
+}
+
+async fn bot_receive_channel_id(bot: Bot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
+    let non_channel_forward = async {
+        bot.send_message(msg.chat.id, "Сообщение должно быть переслано из канала")
+            .await?;
+
+        Ok(())
+    };
+
+    let Some(ForwardedFrom::Chat(Chat {
+        id: channel_id,
+        kind:
+            ChatKind::Public(ChatPublic {
+                title: Some(channel_title),
+                ..
+            }),
+        ..
+    })) = msg.forward_from()
+    else {
+        return non_channel_forward.await;
+    };
+
+    if channel_id.0 >= 0 {
+        return non_channel_forward.await;
+    }
+
+    bot.send_message(
+        msg.chat.id,
+        format!("Посты будут публиковаться в паблик '{channel_title}' ({channel_id})"),
+    )
+    .await?;
+
+    bot.send_message(
+        msg.chat.id,
+        "Напишите, пожалуйста, ссылку на стену сообщества, группы или человека во ВКонтакте",
+    )
+    .await?;
+
+    dialogue
+        .update(BotState::ReceiveVkUrl {
+            channel_id: *channel_id,
+        })
+        .await?;
+
+    Ok(())
+}
+
+async fn bot_receive_vk_url(
+    bot: Bot,
+    dialogue: MyDialogue,
+    posts_channel_id: ChatId,
+    msg: Message,
+) -> HandlerResult {
+    let Some(text) = msg.text() else {
+        bot.send_message(
+            msg.chat.id,
+            "Напишите, пожалуйста, ссылку на стену сообщества, группы или человека во ВКонтакте",
+        )
+        .await?;
+
+        return Ok(());
+    };
+
+    let Ok(url) = Url::parse(text) else {
+        bot.send_message(
+            msg.chat.id,
+            "Напишите, пожалуйста, ссылку на стену сообщества, группы или человека во ВКонтакте",
+        )
+        .await?;
+
+        return Ok(());
+    };
+
+    // Первый символ всегда /
+    let id = url.path().chars().skip(1).collect::<String>();
+
+    bot.send_message(
+        msg.chat.id,
+        format!("Отлично! Посты будут репоститься из vk.com/{id} в {posts_channel_id}"),
+    )
+    .await?;
+
+    dialogue
+        .update(BotState::Active {
+            channel_id: posts_channel_id,
+            vk_id: id,
+        })
+        .await?;
+
+    Ok(())
+}
+
+async fn bot_other(bot: Bot, msg: Message) -> HandlerResult {
+    bot.send_message(msg.chat.id, "Всё настроено").await?;
+    Ok(())
+}
+
+async fn bot_test_post(bot: Bot, (channel_id, vk_id): (ChatId, String)) -> HandlerResult {
+    let post = get_post(&vk_id).await.context("requesting to vk api")?;
+    let post = vk2md2(post);
+
+    bot.send_message(channel_id, post)
+        .parse_mode(teloxide::types::ParseMode::MarkdownV2)
+        .await?;
+
+    Ok(())
+}
+
+async fn get_post(vk_id: &str) -> anyhow::Result<String> {
     const SERVER: &str = "api.vk.com";
     const VERSION: &str = "5.137";
     const LANGUAGE: &str = "ru";
@@ -40,7 +239,7 @@ async fn run() -> anyhow::Result<()> {
         &[
             ("v", VERSION),
             ("lang", LANGUAGE),
-            ("domain", "dar.viardo"),
+            ("domain", vk_id),
             ("offset", "0"),
             ("count", "5"),
         ],
@@ -57,13 +256,18 @@ async fn run() -> anyhow::Result<()> {
         .context("executing wall.get")?;
 
     let response = response
-        .text()
+        .json::<serde_json::Value>()
         .await
         .context("parsing response from wall.get")?;
 
-    fs::write("response.json", response)
-        .await
-        .context("writing response to the file")?;
+    let text = response["response"]["items"][0]["text"]
+        .as_str()
+        .unwrap_or("[Нет текста]")
+        .to_owned();
 
-    Ok(())
+    Ok(text)
+}
+
+fn vk2md2(post: String) -> String {
+    post
 }
